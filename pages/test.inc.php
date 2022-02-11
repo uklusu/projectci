069999ae1219d138f42ef176f1ff0d001f052f91
viktor <uklusu@gmail.com>
2022-02-11 21:38:00 +0200

    6

--- pages/test.inc.php
+++ pages/test.inc.php
@@ -2,5 +2,5 @@
   document::$snippets['title'][] = 'Your head title here';
   document::$snippets['description'] = 'Your meta description here';
 ?>
-<h1>Hello EPAM</h1>
+<h1>Hello EPAM11</h1>
 <p>SOME TEXT  !123</p>
